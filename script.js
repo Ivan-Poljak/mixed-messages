7641430047ad70fc452d1ecdb736ebbb2b4e8a09
Poljak <ipoljak96@gmail.com>
2021-05-30 11:44:12 +0200

    Write TV Shows cases

--- script.js
+++ script.js
@@ -17,3 +17,66 @@ const entertainment = {
 	sci_fi_Series: ['The Mandalorian', 'The 100', 'The X-Files', 'Star Trek', 'Westworld']
 };
 
+//Here will be stored recommended movies or series
+let recommendation = [];
+
+for (let objKeys in entertainment) {
+
+	//Num argument of generateRandom function
+	let numArgument = generateRandomNumber(entertainment[objKeys].length);
+	//Index of element in array
+	let element = entertainment[objKeys][numArgument];
+
+	//Randomly selected element in objects shows key
+	switch(objKeys) {
+		case 'shows':
+			recomendation.push(element);
+		break;
+	}
+
+	//Checks if first element in recommendation array is 'Movies' or 'TV Shows' and pushs randomly pushed objects type key with string
+	if (recommendation[0] == 'Moives') {
+		switch(objKeys) {
+			case 'type':
+				recommendation.push(element + 'Movie');
+			break;
+		}
+	} else if (recommendation[0] == 'TV Shows') {
+		switch(objKeys) {
+			case 'type':
+				recommendation.push(elements + 'TV Shows');
+			break;
+		}
+	}
+
+	//Case movies - choose between types of movies
+	if (recommendation[0] == 'Movies' && recommendation[1] == 'Action Movie') {
+		switch(objKeys) {
+			case 'actionMovies':
+				recommendation.push(element);
+			break;
+		}
+	} else if (recommendation[0] == 'Movies' && recommendation[1] == 'Comedy Movie') {
+		switch(objKeys) {
+			case 'comedyMovies':
+				recommedation.push(element);
+			break;
+		}
+	} else if (recommendation[0] == 'Movies' && recommendation[1] == 'Romance Movie') {
+		switch(objKeys) {
+			case 'romanceMovies':
+				recommendation.push(element);
+			break;
+		}
+	} else if (recommendation[0] == 'Movies' && recommendation[1] == 'Sci-fi Movie') {
+		switch(objKeys) {
+			case 'sci_fi_Movies:
+				recommendation.push(element);
+			break;
+		}
+	}
+
+	//Case TV Shows - choose between types of TV Shows
+
+
+
