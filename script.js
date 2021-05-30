a67513041d01e0a4ca207d1b4bd81038656e1b17
Poljak <ipoljak96@gmail.com>
2021-05-30 10:43:40 +0200

    Create a for\in loop with statements

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateRandomNumber(num) {
+	return Math.floor(Math.random() * num);
+}
+
+const entertainment = {
+	shows: ['Movies', 'TV Shows'],
+	type: ['Action', 'Comedy', 'Romance', 'Sci-fi'],
+
+	actionMovies: ['The Matrix (1999)', 'Die Hard (1998)', 'Rambo First Blood (1982)', 'John Wick (2014)', 'Live Free or Die Hard (2007)'],
+	comedyMovies: ['The Hangover (2009)', 'Grown Ups (2010)', 'Ted (2012)', 'Superbad (2007)', 'Dirty Grandpa (2016)'],
+	romanceMovies: ['Crazy, Stupid, Love (2011)', 'Endless Love (2014)', 'A Star Is Born (2018)', 'La La Land (2016)', 'Life Is Beautiful (1997)'],
+	sci_fi_Movies: ['Star Wars (all of them)', 'Blade Runner 2049 (2017)', 'Interstelar (2014)', 'Avatar (2009)', 'Arrival (2016)'],
+
+	actionSeries: ['NCIS', 'Vikings', 'SEAL Team', 'Prison Break'],
+	comedySeries: ['Seinfeld', 'Two and a Half Men', 'The Fresh Princ of Bel-Air', 'Married.. with Children', 'The Office'],
+	romanceSeries: ['How I Met Your Mother', 'Friends', 'One Tree Hill', 'A Million Little Things', 'This Is Us'],
+	sci_fi_Series: ['The Mandalorian', 'The 100', 'The X-Files', 'Star Trek', 'Westworld']
+};
+
